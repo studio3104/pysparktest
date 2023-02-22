676b71e44b5edad14dd2cb15072ff933a6cd62a7
Satoshi SUZUKI <studio3104.com@gmail.com>
2023-02-22 21:01:54 +0900

    Comment out an unnecessary line that don't affect

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ from app import Hoge
 class TestHoge:
     def test_run(self, mocker: MockerFixture) -> None:
         hoge = Hoge()
-        mocker.patch.object(SparkSession, 'read', return_value=hoge.spark.read, new_callable=mocker.PropertyMock)
+        # mocker.patch.object(SparkSession, 'read', return_value=hoge.spark.read, new_callable=mocker.PropertyMock)
         format_mock = mocker.patch.object(DataFrameReader, 'format', return_value=hoge.spark.read)
         option_mock = mocker.patch.object(DataFrameReader, 'option', return_value=hoge.spark.read)
         load_mock = mocker.patch.object(DataFrameReader, 'load', return_value=hoge.spark.read)
